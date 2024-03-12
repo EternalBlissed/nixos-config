8b980f7773d0f5050723a4e8f1efaa1aac39e6a4
eternalblissed <3tnl@proton.me>
2024-03-12 19:44:58 +1100

    Temp Fix

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -2,6 +2,6 @@
 
 {
   programs.neovim.enable = true;
-  environment.variables.EDITOR = "nvim";
+ # environment.variables.EDITOR = "nvim";
   programs.neovim.extraConfig = lib.fileContents ../../nvim/init.lua;
 }
